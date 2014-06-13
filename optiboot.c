93c10b63ababf0b0d2f03d9504e33dd70f5fc14a
Morten Mjelva <morten.mjelva@nordicsemi.no>
2014-06-13 11:33:59 +0200

    Set initial watchdog timeout to 8 sec

--- optiboot.c
+++ optiboot.c
@@ -519,7 +519,7 @@ static void hardware_init (void)
 #endif
 
   /* Set up watchdog to trigger after 1000ms */
-  watchdogConfig(WATCHDOG_1S);
+  watchdogConfig(WATCHDOG_8S);
 
 #ifdef SOFT_UART
   /* Set TX pin as output */
