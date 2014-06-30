8b02f0ac65c69e1cf7610321fb87bbf0cedeadc8
Mjelva, Morten <morten.mjelva@nordicsemi.no>
2014-06-30 11:05:20 +0200

    Typo fix in test

--- tests/test_ble_with_application.py
+++ tests/test_ble_with_application.py
@@ -16,9 +16,9 @@ dfu_app = 'dfu_application.hex'
 test_app = 'test_application.hex'
 
 avrdude = ['avrdude', '-p', 'm328p', '-P', 'COM4', '-c', 'arduino', '-b', '115200', '-u', '-V']
-flash = ['-U', 'flash:w'+dfu_app]
-verify = ['-U', 'flash:v'+dfu_app]
-verify2 = ['-U', 'flash:v'+test_app]
+flash = ['-U', 'flash:w:'+dfu_app]
+verify = ['-U', 'flash:v:'+dfu_app]
+verify2 = ['-U', 'flash:v:'+test_app]
 
 
 log = open('test.log', 'w')
